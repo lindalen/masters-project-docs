727a88c5c311d3d91fb4153b425cec3fba76bd82
Daniel Lindalen <43276209+lindalen@users.noreply.github.com>
2024-02-11 15:17:35 +0100

    Lazy DB test commit.

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes import router
+import os
+import psycopg2
 
 app = FastAPI()
 
@@ -14,6 +16,30 @@ app.add_middleware(
 )
 
 app.include_router(router)
+
+# Your PostgreSQL connection URL
+connection_url = os.getenv("INTERNAL_DB_URL")
+
+try:
+    # Connect to your PostgreSQL database
+    conn = psycopg2.connect(connection_url)
+    
+    # Create a cursor object
+    cursor = conn.cursor()
+    
+    # Execute a query
+    cursor.execute("SELECT version();")
+    
+    # Fetch and print the result
+    db_version = cursor.fetchone()
+    print("Connected to PostgreSQL database! Version:", db_version)
+    
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
+except Exception as e:
+    print("An error occurred:", e)
+
 
 @app.get("/")
 async def root():
